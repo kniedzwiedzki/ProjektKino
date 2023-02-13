85a1b3e658298a4450ed535a725a9dbe6647ee0c
kniedzwiedzki <niedzwiedzki@adres.pl>
2023-02-13 14:58:10 +0100

    added

---
+++ src/main/java/pl/projekt/alekino/AleKinoApplication.java
@@ -0,0 +1,13 @@
+package pl.projekt.alekino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AleKinoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AleKinoApplication.class, args);
+    }
+
+}
